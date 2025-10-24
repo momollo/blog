e4bcd00f84f045c8128db5d93d863915e5596aa2
morel <maxence.morel2002@gmail.com>
2025-10-24 15:38:07 +0200

    faker2

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
-     protected $fillable = ['title', 'body'];
+     use HasFactory;
+    /**
+     * Les attributs qu’on peut remplir avec create() ou update()
+     */
+    protected $table = 'articles';
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,11 @@ class DatabaseSeeder extends Seeder
          $this->call([
         UserSeeder::class,
     ]);
+
+
+
+{
+    $this->call(PostSeeder::class);
+}
     }
 }
